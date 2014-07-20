7bb8eab4923bb89887a4491c42a7deccebf9ae85
Luciano Mammino <lmammino@oryzone.com>
2014-07-20 12:37:20 +0200

    Added cart models

---
+++ src/Model/Cart/Cart.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace LMammino\EFoundation\Model\Cart;
+
+use LMammino\EFoundation\Model\Order\Order;
+
+/**
+ * Class Cart
+ *
+ * @package LMammino\EFoundation\Model\Cart
+ */
+class Cart extends Order implements CartInterface
+{
+    /**
+     * @var \DateTime $expiresAt
+     */
+    protected $expiresAt;
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getExpiresAt()
+    {
+        return $this->expiresAt;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function setExpiresAt(\DateTime $expiresAt = null)
+    {
+        $this->expiresAt = $expiresAt;
+
+        return $this;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function incrementExpiresAt(\DateInterval $interval = null)
+    {
+        if (null === $this->expiresAt) {
+            $this->expiresAt = new \DateTime();
+        }
+
+        if (null === $interval) {
+            $interval = new \DateInterval('PT3H');
+        }
+
+        $this->expiresAt->add($interval);
+
+        return $this;
+    }
+
+    /**
+     * Checks whether the cart is expired or not.
+     *
+     * @return Boolean
+     */
+    public function isExpired()
+    {
+        if (null === $this->expiresAt) {
+            return false;
+        }
+
+        return $this->expiresAt < (new \DateTime());
+    }
+}
