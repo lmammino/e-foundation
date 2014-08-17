00b3292c11be9b5a3950c458c2812fd1a5684002
Luciano Mammino <lmammino@oryzone.com>
2014-08-18 01:00:00 +0200

    Added other mappings and resolve file

---
+++ src/Address/Resources/config/doctrine/resolve.php
@@ -0,0 +1,7 @@
+<?php
+
+return array(
+    'LMammino\EFoundation\Address\Model\CountryInterface'  => 'LMammino\EFoundation\Address\Model\Country',
+    'LMammino\EFoundation\Address\Model\ProvinceInterface' => 'LMammino\EFoundation\Address\Model\Province',
+    'LMammino\EFoundation\Address\Model\AddressInterface'  => 'LMammino\EFoundation\Address\Model\Address',
+);
