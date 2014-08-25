5ced8e932ab9be4bf513a0db864fdeb54e8bb0eb
Luciano Mammino <lmammino@oryzone.com>
2014-08-25 15:17:32 +0200

    Added test for OptionSubjectTrait

---
+++ tests/Dummy/Variation/Model/DummyOptionSubject.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace LMammino\EFoundation\Tests\Dummy\Variation\Model;
+
+use LMammino\EFoundation\Variation\Model\OptionSubjectInterface;
+use LMammino\EFoundation\Variation\Model\OptionSubjectTrait;
+
+/**
+ * Class DummyOptionSubject
+ *
+ * @package LMammino\EFoundation\Tests\Dummy\Variation\Model
+ */
+class DummyOptionSubject implements OptionSubjectInterface
+{
+    use OptionSubjectTrait;
+}
---
+++ tests/Variation/Model/OptionSubjectTraitTest.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace LMammino\EFoundation\Tests\Variation\Model;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use LMammino\EFoundation\Tests\Dummy\Variation\Model\DummyOptionSubject;
+
+/**
+ * Class OptionSubjectTraitTest
+ *
+ * @package LMammino\EFoundation\Tests\Variation\Model
+ */
+class OptionSubjectTraitTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var DummyOptionSubject $optionSubject
+     */
+    private $optionSubject;
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function setup()
+    {
+        $this->optionSubject = new DummyOptionSubject();
+    }
+
+    /**
+     * @test
+     */
+    public function it_should_handle_options()
+    {
+        $option = $this->getMock('\LMammino\EFoundation\Variation\Model\OptionInterface');
+        $options = new ArrayCollection(array($option));
+        $this->optionSubject->setOptions($options);
+        $this->assertContains($option, $this->optionSubject->getOptions());
+    }
+
+    /**
+     * @test
+     */
+    public function it_should_check_if_has_an_option()
+    {
+        $option = $this->getMock('\LMammino\EFoundation\Variation\Model\OptionInterface');
+        $this->assertFalse($this->optionSubject->hasOption($option));
+        $this->optionSubject->addOption($option);
+        $this->assertTrue($this->optionSubject->hasOption($option));
+    }
+
+    /**
+     * @test
+     */
+    public function it_should_add_an_option()
+    {
+        $option = $this->getMock('\LMammino\EFoundation\Variation\Model\OptionInterface');
+        $this->optionSubject->addOption($option);
+        $this->assertContains($option, $this->optionSubject->getOptions());
+    }
+
+    /**
+     * @test
+     */
+    public function it_should_remove_an_option()
+    {
+        $option = $this->getMock('\LMammino\EFoundation\Variation\Model\OptionInterface');
+        $this->optionSubject->addOption($option);
+        $this->assertContains($option, $this->optionSubject->getOptions());
+        $this->optionSubject->removeOption($option);
+        $this->assertNotContains($option, $this->optionSubject->getOptions());
+    }
+}
